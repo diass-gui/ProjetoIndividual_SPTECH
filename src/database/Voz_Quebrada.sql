975aada2720f5ad5f9358fdab0d3a49c1ac7a897
diass-gui <enriqueguilherme2@gmail.com>
2025-05-24 00:59:46 -0300

    atualização do banco de dados

--- src/database/Voz_Quebrada.sql
+++ src/database/Voz_Quebrada.sql
@@ -24,16 +24,12 @@ CONSTRAINT fkQuiz FOREIGN KEY(fkQuiz)
 CONSTRAINT pkComposta PRIMARY KEY (idPergunta, fkQuiz)
 );
 
-CREATE TABLE Resposta (
-idResposta INT AUTO_INCREMENT,
--- alternativaA VARCHAR(150), 
--- alternativaB VARCHAR(150),
--- alternativaC VARCHAR(150),
-resposta VARCHAR(150), 
-fkPergunta INT, 
-CONSTRAINT fkPergunta FOREIGN KEY (fkPergunta)
-	REFERENCES Perguntas(idPergunta), 
-CONSTRAINT pkCompostaResp PRIMARY KEY(idResposta, fkPergunta)
+CREATE TABLE Alternativa (
+    idAlternativa INT PRIMARY KEY AUTO_INCREMENT,
+    texto VARCHAR(150),
+    correta BOOLEAN,
+    fkPergunta INT,
+    FOREIGN KEY (fkPergunta) REFERENCES Perguntas(idPergunta)
 );
 
 CREATE TABLE Dashboard (
@@ -78,27 +74,78 @@ SELECT quiz.elementoQuiz AS 'Elemento do Quiz',
     ON quiz.idQuiz = p.fkQuiz
     WHERE idQuiz = 1;
 
-INSERT INTO Resposta VALUES 
-(default, "Hip Hop - Cultura de Rua", 1), 
-(default, "Raio X do Brasil", 2), 
-(default, "509-E", 3), 
-(default, "BK", 4), 
-(default, "Mun Rá - do rapper Sabotage", 5), 
-(default, "O rapper Criolo", 6), 
-(default, "GOG", 7), 
-(default, "2003", 8), 
-(default, "Carlos Marighella", 9), 
-(default, "Álbum Sobrevivendo no Inferno, do grupo Racionais MC'S", 10);
+INSERT INTO Alternativa(texto, correta, fkPergunta) VALUES
+("Sobrevivendo no Inferno", false, 1), 
+("Hip Hop - Cultura de Rua", true, 1), 
+("Pergunte A Quem Conhece", false, 1), 
+("Escolha O Seu Caminho", false, 1), 
+("Cores e Valores", false, 2), 
+("Holocausto Urbano", false, 2), 
+("Nada Como Um Dia Após O Outro Dia", false, 2), 
+("Raio X do Brasil", true, 2), 
+("Detentos do Rap", false, 3), 
+("Sistema Negro", false, 3), 
+("509-E", true, 3), 
+("RZO", false, 3), 
+("BK", true, 4), 
+("Djonga", false, 4), 
+("Froid", false, 4), 
+("Don L", false, 4), 
+("Convoque Seu Buda - Criolo", false, 5), 
+("Mil Faces De Um Homem Leal - Racionais", false, 5), 
+("Se Tu Lutas, Tu Conquistas - SNJ", false, 5), 
+("Mun Rá - Sabotage", true, 5), 
+("Criolo", true, 6), 
+("Dexter", false, 6), 
+("Eduardo Taddeo (Facção Central)", false, 6), 
+("Marcelo D2", false, 6), 
+("Edi Rock", false, 7), 
+("GOG", true, 7), 
+("Kaskão", false, 7), 
+("MV Bill", false, 7),
+("2000", false, 8), 
+("1998", false, 8), 
+("2006", false, 8), 
+("2003", true, 8), 
+("Carlos Marighella", true, 9), 
+("Malcolm X", false, 9), 
+("Tim Maia", false, 9), 
+("Jorge Ben Jor", false, 9), 
+("Álbum Sobrevivendo no Inferno, do grupo Racionais MC'S", true, 10), 
+("Álbum Rap É Compromisso, do artista Sabotage", false, 10), 
+("Álbum Provérbios 13, do grupo 509-E", false, 10), 
+("Álbum Castelos & Ruínas, do artista BK", false, 10);
 
-SELECT * FROM Resposta;
+
+-- INSERT INTO Resposta VALUES 
+-- (default, "Hip Hop - Cultura de Rua", 1), 
+-- (default, "Raio X do Brasil", 2), 
+-- (default, "509-E", 3), 
+-- (default, "BK", 4), 
+-- (default, "Mun Rá - do rapper Sabotage", 5), 
+-- (default, "O rapper Criolo", 6), 
+-- (default, "GOG", 7), 
+-- (default, "2003", 8), 
+-- (default, "Carlos Marighella", 9), 
+-- (default, "Álbum Sobrevivendo no Inferno, do grupo Racionais MC'S", 10);
+
+SELECT * FROM Alternativa;
 
 SELECT quiz.elementoQuiz AS 'Elemento do quiz', 
 	p.pergunta AS 'Pergunta', 
-    r.resposta AS 'Resposta'
-    FROM Resposta AS r
-    JOIN Perguntas AS P
-    ON p.idPergunta = r.fkPergunta
+    a.texto AS 'Alternativa', 
+    a.correta AS 'Resposta',
+    (
+    CASE
+		WHEN a.correta = false THEN a.correta = "Resposta Incorreta"
+        WHEN a.correta = true THEN a.correta = "Resposta Correta"
+	END
+    )
+    FROM Alternativa AS a
+    JOIN Perguntas AS p
+    ON p.idPergunta = a.fkPergunta
     JOIN Quiz as quiz
     ON quiz.idQuiz = p.fkQuiz;
 
 SELECT * FROM Usuario;
+
--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 var quizController = require("../controllers/quizController");
 
-// Rota GET que envia as perguntas com respostas
 router.get("/perguntas/:id", function (req, res) {
     quizController.listarPerguntasComRespostas(req, res);
 });
